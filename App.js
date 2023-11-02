4de9a9224c52575257dc9146ffbf1229ee5ffb08
SahjahanReza <32718989+SahjahanReza006c@users.noreply.github.com>
2023-11-02 13:29:13 -0700

    first commit

---
+++ App.js
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  View
+} from 'react-native';
+import Onboard from "./components/Onboard";
+
+
+
+function App() {
+
+  return (
+    <>
+    <View> 
+     
+   
+      
+    </View>
+
+<Onboard/>
+
+</>
+  );
+
+}
+
+
+
+export default App;
+
